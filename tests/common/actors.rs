79e65da6121226f312fc1ba553ff9639c23a56b1
Naja Melan <najamelan@autistici.org>
2019-10-10 12:28:43 +0200

    Add integration tests

--- tests/common/actors.rs
+++ tests/common/actors.rs
@@ -1,11 +1,21 @@
 use super::import::*;
 
 #[ cfg( feature = "remote" ) ]
+
 use
 {
 	serde:: { Serialize, Deserialize },
 };
 
+
+
+
+#[ allow( dead_code ) ]
+//
+#[ derive( Actor ) ] pub struct SumNoSend( pub u64 );
+
+impl !Send for SumNoSend {}
+impl !Sync for SumNoSend {}
 
 
 #[ derive( Actor ) ] pub struct Sum( pub u64 );
@@ -34,6 +44,44 @@ impl Handler< Add > for Sum
 impl Handler< Show > for Sum
 {
 	fn handle( &mut self, _msg: Show ) -> Return<u64> { Box::pin( async move
+	{
+		trace!( "called sum with: Show" );
+
+		self.0
+
+	})}
+}
+
+
+
+impl Handler< Add > for SumNoSend
+{
+	fn handle( &mut self, _: Add ) -> Return<()> { Box::pin( async move
+	{
+		unreachable!( "Can't spawn !Send actor on threadpool" );
+
+	})}
+
+	fn handle_local( &mut self, msg: Add ) -> ReturnNoSend<()> { Box::pin( async move
+	{
+		trace!( "called sum with: {:?}", msg );
+
+		self.0 += msg.0;
+
+	})}
+}
+
+
+
+impl Handler< Show > for SumNoSend
+{
+	fn handle( &mut self, _: Show ) -> Return<u64> { Box::pin( async move
+	{
+		unreachable!( "Can't spawn !Send actor on threadpool" );
+
+	})}
+
+	fn handle_local( &mut self, _msg: Show ) -> ReturnNoSend<u64> { Box::pin( async move
 	{
 		trace!( "called sum with: Show" );
 
